34019ccf4d03a91640364535cb9c5620f0d471b6
mariia-ivanski <mariia.ivanski@gmail.com>
2022-02-01 18:52:02 +0200

    create functions to add new tasks to the list

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
-function addTask(event) {
-  event.preventDefault();
-
-  let newTask = document.querySelector("#input-task");
+function addTask(newTask) {
+  let taskList = document.querySelector("#task-list");
+  let placeholder = document.querySelector("#input-task");
+  taskList.innerHTML += `<li>
+          <input class="form-check-input mt-0" type="checkbox" value="" /><span
+            class="task list-group-item"
+            >${newTask}</span
+          ><button type="button" class="btn-close" aria-label="Delete"></button>
+        </li>`;
+  placeholder.value = ``;
 }
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let newTask = document.querySelector("#input-task");
+  if (!newTask.value.match(/^[A-Z a-z_-]+$/)) {
+    alert("English letters only 😉");
+    let placeholder = document.querySelector("#input-task");
+    placeholder.value = ``;
+  } else {
+    addTask(newTask.value.trim());
+  }
+}
+
+let inputBtn = document.querySelector("#add-task-button");
+inputBtn.addEventListener("click", handleSubmit);
