99a41d372126df6364c3e1c48a0618c8830b9b74
mariia-ivanski <mariia.ivanski@gmail.com>
2022-02-06 19:14:57 +0200

    add add new task and delete task functions

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
-let close = document.getElementsByClassName("delete-btn");
-let i;
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function () {
-    let div = this.parentElement;
-    div.style.display = "none";
-  };
-}
+document.querySelector("#add-task-button").onclick = function () {
+  if (document.querySelector("#input-task").value.length == 0) {
+    alert("Please Enter a Task");
+  } else {
+    document.querySelector("#task-list").innerHTML += `
+            <li class="task">
+                <input class="form-check-input mt-0" type="checkbox" value="" /><span class="task list-group-item" id="taskname">
+                    ${document.querySelector("#input-task").value}
+                </span>
+               <button
+            type="button"
+            class="btn-close delete-btn"
+            aria-label="Delete"
+          ></button>
+            </li>
+        `;
+
+    var current_tasks = document.querySelectorAll(".delete-btn");
+    for (var i = 0; i < current_tasks.length; i++) {
+      current_tasks[i].onclick = function () {
+        this.parentNode.remove();
+      };
+    }
+  }
+};
