f9817939cbb28e6cb70c03a413eb5604824bf5b3
mariia-ivanski <mariia.ivanski@gmail.com>
2022-02-20 23:35:43 +0200

    update index.js to remove element instead of display none

--- src/index.js
+++ src/index.js
@@ -1,7 +1,30 @@
+/*function manageTasks() {
+  let checkbox = document.querySelectorAll("input[type=checkbox]");
+  let taskDone = document.querySelectorAll(".task");
+  for (let x = 0; x < checkbox.length; x++) {
+    checkbox[x].addEventListener("change", function () {
+      if (checkbox[x].checked == true) {
+        taskDone[x].classList.add(`isDone`);
+        checkbox[x].setAttribute("checked", "checked");
+      } else {
+        taskDone[x].classList.remove(`isDone`);
+      }
+    });
+  }
+
+  let current_tasks = document.querySelectorAll(".delete-btn");
+  for (let i = 0; i < current_tasks.length; i++) {
+    current_tasks[i].onclick = function () {
+      this.parentNode.remove();
+    };
+  }
+}
+*/
+
 var myNodelist = document.getElementsByTagName("LI");
 var i;
 for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
+  var span = document.createElement("button");
   var txt = document.createTextNode("\u00D7");
   span.className = "close delete-btn";
   span.appendChild(txt);
@@ -13,7 +36,7 @@ var i;
 for (i = 0; i < close.length; i++) {
   close[i].onclick = function () {
     var div = this.parentElement;
-    div.style.display = "none";
+    div.remove();
   };
 }
 
@@ -53,7 +76,7 @@ function newElement(e) {
   }
   document.getElementById("input-task").value = "";
 
-  var span = document.createElement("SPAN");
+  var span = document.createElement("button");
   var txt = document.createTextNode("\u00D7");
   span.className = "close delete-btn";
   span.appendChild(txt);
@@ -62,10 +85,41 @@ function newElement(e) {
   for (i = 0; i < close.length; i++) {
     close[i].onclick = function () {
       var div = this.parentElement;
-      div.style.display = "none";
+      div.remove();
     };
   }
 }
 
 let form = document.getElementById("tasks-form");
 form.addEventListener("submit", newElement);
+
+/*
+function addNewTask(event) {
+  event.preventDefault();
+  let newTask = document.querySelector("#input-task").value.trim();
+  if (newTask.length === 0) {
+    alert("Please Enter a Task");
+  } else {
+    document.querySelector("#task-list").innerHTML += `
+            <li>
+                <input class="form-check-input mt-0" type="checkbox" name="checkbox" value="" /><label for="checkbox" class="task list-group-item" id="taskname">
+                    ${newTask}
+                </label>
+               <button
+            type="button"
+            class="btn-close delete-btn"
+            aria-label="Delete"
+          ></button>
+            </li>
+        `;
+    let placeholder = document.querySelector("#input-task");
+    placeholder.value = "";
+  }
+
+  manageTasks();
+}
+
+let form = document.querySelector("#tasks-form");
+form.addEventListener("submit", addNewTask);
+*/
+//manageTasks();
