45c18e467c0d1fd8a95b4c4cefcf271766a1a9d7
mariia-ivanski <mariia.ivanski@gmail.com>
2022-02-23 10:32:21 +0200

    remove commented out old code from index.js

--- src/index.js
+++ src/index.js
@@ -1,26 +1,3 @@
-/*function manageTasks() {
-  let checkbox = document.querySelectorAll("input[type=checkbox]");
-  let taskDone = document.querySelectorAll(".task");
-  for (let x = 0; x < checkbox.length; x++) {
-    checkbox[x].addEventListener("change", function () {
-      if (checkbox[x].checked == true) {
-        taskDone[x].classList.add(`isDone`);
-        checkbox[x].setAttribute("checked", "checked");
-      } else {
-        taskDone[x].classList.remove(`isDone`);
-      }
-    });
-  }
-
-  let current_tasks = document.querySelectorAll(".delete-btn");
-  for (let i = 0; i < current_tasks.length; i++) {
-    current_tasks[i].onclick = function () {
-      this.parentNode.remove();
-    };
-  }
-}
-*/
-
 var myNodelist = document.getElementsByTagName("LI");
 var i;
 for (i = 0; i < myNodelist.length; i++) {
@@ -92,34 +69,3 @@ function newElement(e) {
 
 let form = document.getElementById("tasks-form");
 form.addEventListener("submit", newElement);
-
-/*
-function addNewTask(event) {
-  event.preventDefault();
-  let newTask = document.querySelector("#input-task").value.trim();
-  if (newTask.length === 0) {
-    alert("Please Enter a Task");
-  } else {
-    document.querySelector("#task-list").innerHTML += `
-            <li>
-                <input class="form-check-input mt-0" type="checkbox" name="checkbox" value="" /><label for="checkbox" class="task list-group-item" id="taskname">
-                    ${newTask}
-                </label>
-               <button
-            type="button"
-            class="btn-close delete-btn"
-            aria-label="Delete"
-          ></button>
-            </li>
-        `;
-    let placeholder = document.querySelector("#input-task");
-    placeholder.value = "";
-  }
-
-  manageTasks();
-}
-
-let form = document.querySelector("#tasks-form");
-form.addEventListener("submit", addNewTask);
-*/
-//manageTasks();
