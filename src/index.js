6025d3e53eec04c1e0ebafc466100e21d4955ac8
mariia-ivanski <mariia.ivanski@gmail.com>
2022-02-27 13:31:39 +0200

    update index.js - work on local storage function

--- src/index.js
+++ src/index.js
@@ -1,20 +1,61 @@
-let taskList;
-let inputValue = document.getElementById("input-task").value;
-!localStorage.tasks
-  ? (tasks = [])
-  : (tasks = JSON.parse(localStorage.getItem("tasks")));
+let taskList = JSON.parse(localStorage.getItem("tasks")) || [];
+taskList.forEach(newElement);
 
-function Task(inputValue) {
-  this.inputValue = inputValue;
-  this.completed = false;
+function newElement(inputValue) {
+  let li = document.createElement("li");
+  let inputField = document.createElement("INPUT");
+  inputField.setAttribute("class", "form-check-input mt-0");
+  inputField.setAttribute("type", "checkbox");
+  inputField.setAttribute("name", "checkbox");
+  inputField.setAttribute("value", "");
+
+  var span = document.createElement("SPAN");
+  let t = document.createTextNode(inputValue);
+  span.setAttribute("class", "task list-group-item");
+
+  li.appendChild(inputField);
+  span.appendChild(t);
+  li.appendChild(span);
+  document.getElementById("task-list").appendChild(li);
+
+  var span = document.createElement("button");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close delete-btn";
+  span.appendChild(txt);
+  li.appendChild(span);
+  span.addEventListener("click", (e) => {
+    li.parentNode.removeChild(li);
+    localStorage.removeItem(li);
+  });
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+      var div = this.parentElement;
+      div.remove();
+    };
+  }
+  document.getElementById("input-task").value = "";
 }
 
-const updateLocal = () => {
-  localStorage.setItem("tasks", JSON.stringify(taskList));
-};
+function handleSubmit(event) {
+  event.preventDefault();
+  let inputValue = document.getElementById("input-task").value;
+
+  if (inputValue === "") {
+    alert("You must write something!");
+  } else {
+    function Task(inputValue) {
+      this.inputValue = inputValue;
+    }
+    taskList.push(new Task(inputValue));
+    localStorage.setItem("tasks", JSON.stringify(taskList));
+    inputValue = "";
+    newElement(inputValue);
+  }
+}
 
 console.log(taskList);
-
+/*
 var myNodelist = document.getElementsByTagName("LI");
 var i;
 for (i = 0; i < myNodelist.length; i++) {
@@ -33,7 +74,7 @@ for (i = 0; i < close.length; i++) {
     div.remove();
   };
 }
-
+*/
 var list = document.querySelector("ul");
 list.addEventListener(
   "click",
@@ -46,45 +87,5 @@ list.addEventListener(
   false
 );
 
-function newElement() {
-  if (inputValue === "") {
-    alert("You must write something!");
-  } else {
-    let li = document.createElement("li");
-    let inputField = document.createElement("INPUT");
-    inputField.setAttribute("class", "form-check-input mt-0");
-    inputField.setAttribute("type", "checkbox");
-    inputField.setAttribute("name", "checkbox");
-    inputField.setAttribute("value", "");
-
-    var span = document.createElement("SPAN");
-    let t = document.createTextNode(inputValue);
-    span.setAttribute("class", "task list-group-item");
-
-    li.appendChild(inputField);
-    span.appendChild(t);
-    li.appendChild(span);
-    document.getElementById("task-list").appendChild(li);
-  }
-  document.getElementById("input-task").value = "";
-
-  var span = document.createElement("button");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close delete-btn";
-  span.appendChild(txt);
-  li.appendChild(span);
-
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function () {
-      var div = this.parentElement;
-      div.remove();
-    };
-  }
-}
-
 let form = document.getElementById("tasks-form");
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  taskList.push(new Task(inputValue));
-  updateLocal();
-});
+form.addEventListener("submit", handleSubmit);
